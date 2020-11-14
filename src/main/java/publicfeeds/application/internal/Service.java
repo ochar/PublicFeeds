155002751a4f73a3fa8e5e53fc8ea97613c64073
io <io@x301>
2020-11-14 18:07:01 +0700

    add - Methods for item like and comment in service class

--- src/main/java/publicfeeds/application/internal/Service.java
+++ src/main/java/publicfeeds/application/internal/Service.java
@@ -10,8 +10,12 @@ import java.util.Optional;
 import static java.util.stream.Collectors.toList;
 import org.springframework.beans.factory.annotation.Autowired;
 import publicfeeds.application.internal.jpa.AuthorRepository;
+import publicfeeds.application.internal.jpa.ItemCommentRepository;
+import publicfeeds.application.internal.jpa.ItemLikeReposistory;
 import publicfeeds.application.internal.jpa.ItemRepository;
 import publicfeeds.domain.Item;
+import publicfeeds.domain.ItemComment;
+import publicfeeds.domain.ItemLike;
 
 /**
  *
@@ -22,6 +26,8 @@ public class Service {
 	
 	@Autowired private ItemRepository itemRepo;
 	@Autowired private AuthorRepository authorRepo;
+	@Autowired private ItemLikeReposistory likeRepo;
+	@Autowired private ItemCommentRepository commentRepo;
 	
 	
 	public Optional<Item> getItemById(String id) {
@@ -67,6 +73,92 @@ public class Service {
 	
 	public List<Item> getItemsByAuthor(String authorId) {
 		return itemRepo.findByAuthorId(authorId);
+	}
+	
+	
+	public List<ItemLike> getLikesByItemId(String itemId) {
+		return likeRepo.findByItemId(itemId);
+	}
+	
+	public List<ItemLike> getLikesByUsername(String username) {
+		return likeRepo.findByUsername(username);
+	}
+	
+	public ItemLike likeAnItem(String itemId, String username) {
+		Optional<Item> foundItem = getItemById(itemId);
+		
+		if (foundItem.isPresent()) {
+			List<ItemLike> foundLikes = likeRepo.findByItemId(foundItem.get().getId());
+			Optional<ItemLike> foundAny = foundLikes.stream()
+					.filter(like -> like.getUsername().equals(username))
+					.findAny();
+			
+			if (foundAny.isPresent()) {
+				return foundAny.get();
+			} else {
+				return likeRepo.save(new ItemLike(foundItem.get(), username));
+			}
+			
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean unlikeAnItem(String itemId, String username) {
+		Optional<Item> foundItem = getItemById(itemId);
+		
+		if (foundItem.isPresent()) {
+			List<ItemLike> foundLikes = likeRepo.findByItemId(foundItem.get().getId());
+			Optional<ItemLike> foundAny = foundLikes.stream()
+					.filter(like -> like.getUsername().equals(username))
+					.findAny();
+			
+			if (foundAny.isPresent()) {
+				likeRepo.delete(foundAny.get());
+			}
+			return true;
+			
+		} else {
+			return false;
+		}
+	}
+	
+	public List<ItemComment> getCommentsByItemId(String itemId) {
+		return commentRepo.findByItemId(itemId);
+	}
+	
+	public List<ItemComment> getCommentsByUsername(String username) {
+		return commentRepo.findByUsername(username);
+	}
+	
+	public ItemComment postCommentToItem(String content, String itemId, String username) {
+		Optional<Item> foundItem = getItemById(itemId);
+		if (foundItem.isPresent()) {
+			return commentRepo.save(new ItemComment(content, foundItem.get(), username));
+		} else {
+			return null;
+		}
+	}
+	
+	public ItemComment updateComment(String content, long commentId) {
+		Optional<ItemComment> foundComment = commentRepo.findById(commentId);
+		if (foundComment.isPresent()) {
+			ItemComment ic = foundComment.get();
+			ic.setContent(content);
+			return commentRepo.save(ic);
+			
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean deleteComment(long commentId) {
+		if (commentRepo.findById(commentId).isPresent()) {
+			commentRepo.deleteById(commentId);
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	
