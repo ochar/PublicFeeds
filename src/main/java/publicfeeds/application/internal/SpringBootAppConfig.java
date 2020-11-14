356083a1708cbfe697580d5e601cad54b6754c51
io <io@x301>
2020-11-14 07:12:12 +0700

    initial c - spring boot main class, config, and maven pom

---
+++ src/main/java/publicfeeds/application/internal/SpringBootAppConfig.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package publicfeeds.application.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author io
+ */
+@SpringBootApplication(scanBasePackages = "publicfeeds")
+public class SpringBootAppConfig {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SpringBootAppConfig.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
+	}
+
+}
